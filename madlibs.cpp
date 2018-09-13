a0f2d01ce4c6c3d93d035a0fbccfba9d4e0df5ce
rahoofard <rahoofard@mail.csuchico.edu>
2018-09-12 17:07:39 -0700

    I added a bunch of inputs so that someone could change the set story as they like. I gave them the basic outline but they were prompted to fill in the missing bits to finish it.

--- madlibs.cpp
+++ madlibs.cpp
@@ -1,9 +1,28 @@
 //Authors: 
 #include <iostream>
 #include <string>
+using namespace std;
+
 
 int main()
 {
+int years;
+string name;
+string noun;
+string verb;
 
+
+cout<<"Yesterday"<<endl;
+cout<<"Please choose a name"<<endl;
+cin>> name;
+cout<<"came to my house and started banging on the front door screaming.  I looked around and nobody was there, but out of the corner of my eye I saw a "<< endl;
+cout<<"Please choose a noun"<<endl;
+cin>> noun;
+cout<<"The " <<endl;
+cout<< noun;
+cout<<" tried "<<endl;
+cout<<"Please choose a verb"<<endl;
+cin>> verb;
+cout<< name <<endl;
   return 0;
 }
